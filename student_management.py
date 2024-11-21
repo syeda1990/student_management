8eba6d7967ddd78059c47800a917782bac04e709
syeda siddiqua <syeda.siddiqua.bebe@gmail.com>
2024-11-21 12:18:20 +0100

    added a while loop choice

---
+++ student_management.py
@@ -0,0 +1,24 @@
+while (True):
+    print("\n Student Management System")
+    print("1. Add a new student")
+    print("2. View all students")
+    print("3. Update a student information")
+    print("4. Remove a student information")
+    print("5. Save to a File and Exit")
+
+    choice=int(input("Select a option from 1-5 "))
+
+    if choice ==1 :
+        pass
+    elif choice ==2:
+        pass
+    elif choice ==3:
+        pass
+    elif choice ==4:
+        pass
+    elif choice ==5:
+        pass
+        break
+    else:
+        print("Please enter a valid choice")
+
