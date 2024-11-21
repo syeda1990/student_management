be581a5cfd73f3c3bba88612cceaa2c7c90d9a63
syeda siddiqua <syeda.siddiqua.bebe@gmail.com>
2024-11-21 12:44:28 +0100

    i added one more class and a gave a rough structure

--- student_management.py
+++ student_management.py
@@ -1,3 +1,4 @@
+# class handels student information 
 class Student:
     def __init__(self):
         pass
@@ -5,13 +6,30 @@ class Student:
         pass
     def display_all_students(self):
         pass
-    
+
+# class that will do student mangament functions
+class StudentManagement:
+    def __init__(slef):
+        pass
+    def add_student(self):
+        pass
+    def view_student(self):
+        pass
+    def update_student(self):
+        pass
+    def delete_student(self):
+        pass
+    def save_students_to_file(self):
+        pass
+    def load_students_from_file(self):
+        pass
 
 
 
 
 
-
+# the main program begins
+system=StudentManagement()
 while (True):
     print("\n Student Management System")
     print("1. Add a new student")
@@ -23,15 +41,15 @@ while (True):
     choice=int(input("Select a option from 1-5 "))
 
     if choice ==1 :
-        pass
+        system.add_student()
     elif choice ==2:
-        pass
+        system.view_student()
     elif choice ==3:
-        pass
+        system.update_student()
     elif choice ==4:
-        pass
+        system.delete_student()
     elif choice ==5:
-        pass
+        system.save_students_to_file()
         break
     else:
         print("Please enter a valid choice")
