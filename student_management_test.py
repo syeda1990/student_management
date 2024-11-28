5111c722935a2d5daba8168b356d537070ce3477
syeda siddiqua <syeda.siddiqua.bebe@gmail.com>
2024-11-28 12:24:57 +0100

    did a change in unit test

--- student_management_test.py
+++ student_management_test.py
@@ -10,9 +10,9 @@ class TestStudentManagement(unittest.TestCase):
         self.system.add_student(1, "Zara Fathima", 20, "VG", ["Math", "Science"]) 
         self.assertEqual(len(self.system.students), 1) 
 
-    ''' def test_view_student(self):
+    def test_view_student(self):
         pass
-    def test_update_student(self):
+    '''def test_update_student(self):
         pass
     def test_delete_student(self):
         pass
