8e89f2a3b90ca18922c483a4c8e8bd8957f432f7
syeda siddiqua <syeda.siddiqua.bebe@gmail.com>
2024-11-28 11:30:26 +0100

    created unittest rough structure

---
+++ student_management_test.py
@@ -0,0 +1,20 @@
+import unittest
+from student_management import StudentManagement
+
+class TestStudentManagement(unittest.TestCase):
+    def test_add_student(self):
+        pass
+    def test_view_student(self):
+        pass
+    def test_update_student(self):
+        pass
+    def test_delete_student(self):
+        pass
+    def test_save_students_to_file(self):
+        pass
+    def test_save_students_from_file(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
+    
